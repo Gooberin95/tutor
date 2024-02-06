70d9c87714ddcf11a4aa98dc13e5a3d3c8485c65
Brandon Ruiz <Sober4202020@outlook.com>
2024-02-06 04:26:32 +0000

    addding

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,18 @@
 import React from 'react';
-import microsoftLogo from './mavisOne.jpg';
-import azureinfoOne from './mavisTwo.jpg';
-import azureinfoTwo from './mavisThree.jpg';
 import coolPattern from './coolPattern.jpg';
-import azure from './mavisFour.jpg';
-import profilePic from './mavisFive.jpg'
-import logoTwo from './mavisSix.jpg';
 import eventPic from './eventPic.jpg';
-import Image from 'react-bootstrap/Image';
 import personalSummaryOne from './personalSummaryOne.js';
 import personalSummaryTwo from './personalSummaryTwo.js';
 import personalSummaryThree from './personalSummaryThree.js';
-import fashionOne from './fashionOne.jpg';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
 const Home = () => {
-		  const parallaxStyle = {
-			  		      backgroundImage: `url(${fashionOne})`,
-			  		      minHeight: '500px',
-			  		      backgroundAttachment: 'fixed',
-			  		      backgroundPosition: 'center',
-			  		      backgroundRepeat: 'no-repeat',
-			  		      backgroundSize: 'cover',
-			  		    };
 		const parallaxStyleTwo = {
 					
-					      backgroundImage: `url(${coolPattern})`,
+					      backgroundImage: `url(${eventPic})`,
 					      minHeight: '500px',
 					      backgroundAttachment: 'fixed',
 					      backgroundPosition: 'center',
@@ -38,7 +22,7 @@ const Home = () => {
 
 			 const parallaxStyleThree = {
 				 		
-				 			 backgroundImage: `url(${eventPic})`,
+				 			 backgroundImage: `url(${coolPattern})`,
 				 		      minHeight: '500px',
 				 		      backgroundAttachment: 'fixed',
 				 		      backgroundPosition: 'center',
@@ -51,17 +35,14 @@ const Home = () => {
 		  return (
 			  		
 			  		      <div>
-			  		        <div style={parallaxStyle} className="parallax" ></div>
-			  				<Container fluid id="section1">
+			  				<Container fluid id="section1" style={{marginTop: "95px", marginBottom: "75px"}}>
 			  		  	<Row>
 			  		 	<Col style={{height:"500px"}} className="d-flex justify-content-evenly align-items-center">
-			  				  <Image height="150px" src={logoTwo}  />
-			  				  <Image height="300px" width="250px" style={{objectFit: "cover"}} rounded src={profilePic}/>
+			  				<h3>{personalSummaryOne}</h3>
+
 			  		  	</Col>
 			  		  	<Col className="d-flex justify-content-center align-items-center" >
 			  		  		<Col>
-			  		  	       <p>{personalSummaryOne}</p>
-			  		  	       <p>{personalSummaryTwo}</p>
 			  		  	       <p>{personalSummaryThree}</p>
 			  		  </Col>
 			  		  	</Col>
@@ -69,27 +50,33 @@ const Home = () => {
 			  			</Container>
 			  		   	
 			  		  <div style={parallaxStyleTwo} className="parallax" id="section2"></div>
-			  		 			<Container  id="section2" fluid>
+			  		 			<Container  id="section2" fluid style={{marginTop: "75px", marginBottom: "75px"}}>
 			  		  	<Row>
 			  		  	<Col className="d-flex justify-content-center align-items-center" >
 			  		  		<Col>
-			  		  	       <p>{azureinfoOne}</p>
-			  		  	       <p>{azureinfoTwo}</p>
+			  		  	       <h5>{personalSummaryThree}</h5>
 			  		  </Col> 
 			  		  <Col style={{height:"500px"}} className="d-flex justify-content-evenly align-items-center">
-			  				  <Image height="150px" src={azure}  />
-			  				  <Image height="250px" width="350px" style={{objectFit: "cover"}} rounded src={microsoftLogo}/>
+			  				<h3>{personalSummaryTwo}</h3>
 			  		  	</Col>
 			  		  	</Col>
 			  		  	</Row>
 			  			</Container>
 			            
 			  		  <div style={parallaxStyleThree} className="parallax"></div>
-			  			<Container fluid id="section3" >
+			  			<Container fluid id="section3" style={{marginTop: "75px"}}>
+			  			<Row>
+			  				<Col>
+			  					<h3>{personalSummaryOne}</h3>
+			  				</Col>
+			  				<Col>
+			  					<h5>{personalSummaryThree}</h5>
+			  				</Col>
+			  			</Row>
 			  				</Container>
-			  		  <Container fluid style={{height: "50px"}}>
-			  		  </Container>
+			  		
 			  		      </div>
+			  		
 			  		    );
 };
 
