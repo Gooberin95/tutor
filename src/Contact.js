6d8c1badccc5903813eecb6b5f0c285ef9471ad4
Brandon Ruiz <Sober4202020@outlook.com>
2024-02-07 01:17:35 +0000

    adding

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,6 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import formBackground from './formBackground.jpg';
 function Contact() {
 	const labels = {
 		fontWeight: "bold"
